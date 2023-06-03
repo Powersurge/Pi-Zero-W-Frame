21fbbe9bb9c8b2a3f1b4ace48620cbb86a960d2b
Powersurge <Powersurge@users.noreply.github.com>
2023-06-03 16:57:55 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #########################################################################
 #Pi Zero W Frame                                                        #
-#This script will install Apache, PHP, and MySQL.                       #
+#This script will install Apache, PHP, and SabreDAV WebDAV client.      #
 #This script was written by Chris Newell                                #
 #[C] 2023 Chris Newell: See LICENSE.md for details                      #
 #########################################################################
 
 #!/bin/bash
+ipaddress="$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')";
 
 #Prerequisites
 sudo apt-get update
@@ -15,10 +16,18 @@ sudo apt-get install -y apache2
 sudo echo "ServerName localhost" >> /etc/apache2/httpd.conf
 
 #PHP
-sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite
+sudo apt-get install -y php libapache2-mod-php
 
-#MySQL
-sudo apt-get install -y mysql-server mysql-client php5-mysql
+#SabreDAV WebDAV client
+sudo apt-get install -y composer
+composer require sabre/dav
 
-#Additional Dependencies
-sudo apt-get install -y nmap zenmap
+#Copy files from TAR file
+sudo tar xvjf pizerowframe.tar.bz2 --no-overwrite-dir -C /
+
+#Almost done!
+echo
+echo Pi Zero W Frame can now be configured with your web browser pointed at http://$ipaddress. Use 'pizerowframe' to log in and be sure to change the password!
+echo
+read -p "Press [Enter] key to continue..."
+echo

46261707c7a8d7b5bcd136f4fc23c9daa3a37e88
Powersurge <Powersurge@users.noreply.github.com>
2023-06-04 01:04:56 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@ sudo chown -R www-data:www-data /var/www/html
 sudo apt-get install -y php libapache2-mod-php
 
 #Composer
-php -r “copy(‘https://getcomposer.org/installer’, ‘composer-setup.php’);”
-php -r “if (hash_file(‘sha384’, ‘composer-setup.php’) === ‘906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8’) { echo ‘Installer verified’; } else { echo ‘Installer corrupt’; unlink(‘composer-setup.php’); } echo PHP_EOL;”
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 
 #SabreDAV WebDAV client
